7f59d375fba9ae7a98fc70f65bc80fb95b703f57
Zachery Anderson <zanderson@student.neumont.edu>
2019-02-25 20:03:00 -0700

    Created Point and LineSegments
    Got my library beginning to work in js

---
+++ Angular/server/library/Logic.js
@@ -0,0 +1,12 @@
+var LineSegment = require("./LineSegment");
+var Point = require("./Point");
+
+//Testing the creation of Points and LineSegments
+// var p1 = Point.createPoint(5, 6);
+// var p2 = Point.createPoint(2, 9);
+
+// var line = LineSegment.createBothEnds(p1, p2);
+// console.log("This is the line itself: " + line.toString());
+
+// console.log("This is the left point: " + line.left.toString());
+// console.log("This is the right point: " + line.right.toString());
