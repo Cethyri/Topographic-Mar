6d26e20e780025624a5e4fc079da848f2bf710c0
Wesley Barlow <sirwesleybarlow@gmail.com>
2019-02-26 19:36:43 -0700

    Complete user authentication

--- Angular/src/app/components/gallery/gallery.component.spec.ts
+++ Angular/src/app/components/gallery/gallery.component.spec.ts
@@ -1,26 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import firebase from "firebase";
-
-var config = {
-  apiKey: "AIzaSyAC-6lFDwyb1K99UuPIqz7MVEjowrVcHu0",
-  authDomain: "topographic-mar.firebaseapp.com",
-  databaseURL: "https://topographic-mar.firebaseio.com",
-  storageBucket: "topographic-mar.appspot.com",
-};
-
-var provider = new firebase.auth.GoogleAuthProvider();
-firebase.auth().signInWithRedirect(provider);
-
-firebase.initializeApp(config);
-
-// firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-//   // Handle Errors here.
-//   var errorCode = error.code;
-//   var errorMessage = error.message;
-//   // ...
-// });
-
 import { GalleryComponent } from './gallery.component';
 
 describe('GalleryComponent', () => {
--- Angular/src/app/components/test-db-ui/test-db-ui.component.spec.ts
+++ Angular/src/app/components/test-db-ui/test-db-ui.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TestDbUiComponent } from './test-db-ui.component';
 
 describe('TestDbUiComponent', () => {
