a6346e49b0c5b6afb0ee0c1d4167625e53dbd0aa
Wesley Barlow <sirwesleybarlow@gmail.com>
2019-02-26 18:46:12 -0700

    Add Firebase authentication

--- Angular/src/app/components/game-screen/game-screen.component.spec.ts
+++ Angular/src/app/components/game-screen/game-screen.component.spec.ts
@@ -1,6 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GameScreenComponent } from './game-screen.component';
+import firebase from "firebase";
+
+var config = {
+  apiKey: "AIzaSyAC-6lFDwyb1K99UuPIqz7MVEjowrVcHu0",
+  authDomain: "topographic-mar.firebaseapp.com",
+  databaseURL: "https://topographic-mar.firebaseio.com",
+  storageBucket: "topographic-mar.appspot.com",
+};
+
+var provider = new firebase.auth.GoogleAuthProvider();
+firebase.auth().signInWithRedirect(provider);
+
+firebase.initializeApp(config);
+
+// firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+//   // Handle Errors here.
+//   var errorCode = error.code;
+//   var errorMessage = error.message;
+//   // ...
+// });
+
 
 describe('GameScreenComponent', () => {
   let component: GameScreenComponent;
